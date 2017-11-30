a9cc56dbe8ed269db19b271a95411a82725e8ea9
cheekyorkind <gkbn7y7h>
2017-11-30 15:26:32 +0900

    프로그래스 바 추가

--- BusinessLogics/front.py
+++ BusinessLogics/front.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 import sys
 
-from PyQt5.QtWidgets import QWidget, QPushButton, QGroupBox, QAbstractItemView, QBoxLayout, QListView, QApplication, QFileDialog  
+from PyQt5.QtWidgets import QWidget, QPushButton, QGroupBox, QAbstractItemView, QBoxLayout, QListView, QFileDialog, QProgressBar  
 
 from PyQt5.QtGui import QStandardItemModel, QStandardItem 
 
-from PyQt5.QtCore import Qt, pyqtSlot
+from PyQt5.QtCore import Qt, pyqtSlot, QBasicTimer
 
 from searchFiles import SearchFiles
 from PackingTest import PackingTest
@@ -23,22 +23,36 @@ class Form(QWidget):
         
         # 결과 출력을 QListView로 한다.
         grp_QListView = QGroupBox("결과 출력")
-        self.listView = QListView(self)
-
-        self.listView.setEditTriggers(QAbstractItemView.NoEditTriggers)
         layout = QBoxLayout(QBoxLayout.TopToBottom)
+        
+        self.listView = QListView(self)
+        self.listView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        
         layout.addWidget(self.listView)
         grp_QListView.setLayout(layout)
+
+        # 프로그래스 바 레이아웃
+        grp_QProgressBar = QGroupBox("진행도")
+        grp_QProgressBar_layout = QBoxLayout(QBoxLayout.TopToBottom)
+         
+        self.pbar = QProgressBar(self)
+ 
+        grp_QProgressBar_layout.addWidget(self.pbar)
+        grp_QProgressBar.setLayout(grp_QProgressBar_layout)
         
         # 확인 버튼 누르면 QListView에 결과를 표시한다.
         self.confirm_btn = QPushButton("확인")
 
         layout_base.addWidget(grp_QListView)
-#         layout_base.addWidget(grp_QLineEdits)
+        layout_base.addWidget(grp_QProgressBar)
         layout_base.addWidget(self.confirm_btn)
 
 
-        self._PackingTest = PackingTest();
+        self.progressTimer = QBasicTimer(); # 프로그래스 바에 쓰는 타이머 인스턴스
+        self.timerCounter = 0;  # 프로그래스 바 진행상황 표시에 사용할 변수
+        
+        self._PackingTest = PackingTest();  # 패킹 테스트하는 인스턴스
+        
         self.init_signals()
         self.show()
         
@@ -47,28 +61,37 @@ class Form(QWidget):
   
     @pyqtSlot()
     def on_click(self):
+        
         DirectoryPath = self.selectDirectory();
-
+ 
         # UI의 X를 눌렀을때 None이 DirectoryPath에 들어가서 에러나기 때문에 추가한다.
         if len(DirectoryPath) == 0: return ;
-
+ 
         self.itemModel = QStandardItemModel();
         fileList = SearchFiles().travelingFolders(DirectoryPath);
+        fileListLength = len(fileList);
+
+        # 프로그래스 바 타이머 시작
+        self.progressTimer.start(100, self);
+         
+        if fileListLength == 0: self.pbar.setValue(100); return ;
+         
         testResult = [];
-        
-        
-        
-        # testResult = {'entropies': 0, 'packedFile': ''} 형태이다.    
+        currentLoopCounter = 0; # 프로그래스 바의 진행 비율을 측정하기 위한 변수이다.
+            
         for i in fileList:
-            tmpResult = self._PackingTest.startReadAll(i);
+            tmpResult = self._PackingTest.startReadAll(i);  # testResult = {'entropies': 0, 'packedFile': ''} 형태이다.
+            currentLoopCounter += 1;
+            
+            self.timerCounter = 100 * currentLoopCounter / fileListLength;
+            self.pbar.setValue(self.timerCounter);
+            
             testResult.append(self._PackingTest.startReadAll(i));
-#             
+             
             resultStr = str(tmpResult['entropies']) + ' <- ' +  tmpResult['packedFile'];
-            
+             
             self.itemModel.appendRow(QStandardItem(resultStr));
-#             self.itemModel.appendRow(QStandardItem(str(i)));
-            
-            
+             
         self.listView.setModel(self.itemModel);
 
     # 디렉토리 선택창을 띄우고 선택한 디렉토리의 경로를 반환한다.
@@ -76,3 +99,10 @@ class Form(QWidget):
         dialog = QFileDialog()
         DirectoryPath = dialog.getExistingDirectory(None, "Select Folder")
         return DirectoryPath;
+
+    # 시간에 따른 프로그래스 바의 진행도를 측정하고 종료시키는 함수이다.    
+    def timerEvent(self, e):
+        if self.timerCounter >= 100:
+            self.progressTimer.stop()
+            return
+
--- BusinessLogics/main.py
+++ BusinessLogics/main.py
@@ -1,15 +1,11 @@
 # coding: utf-8
 import sys
 
-import sys
-from PyQt5.QtWidgets import QWidget, QToolTip, QPushButton, QApplication
-from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication
+
 from front import Form
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     form = Form();
     sys.exit(app.exec_())
-#         self.resize(250, 150)
-#         self.move(300, 300)
-#         self.setWindowTitle('Simple')
