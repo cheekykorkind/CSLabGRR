d6bb4b619800599f639461a6e6195eee3268498d
cheekyorkind <gkbn7y7h>
2017-12-26 21:26:23 +0900

    진입점 섹션의 범위를 addres of entry point가 있는 섹션으로 수정했다.

--- BusinessLogics/PackingTest.py
+++ BusinessLogics/PackingTest.py
@@ -1,28 +1,24 @@
 # coding: utf-8
-import sys
-import re
-import os, io
+
 import math
 from collections import Counter
-import time
-import itertools
 
 '''
 start함수
 파일 1개에 대해서 논문 플로우차트 내용대로 구현함.
-1. PE파일여부 판단하기.(MZ시그니쳐 확인)
-2. IMAGE_SCN_MEM_WRITE의 값이 0x80이상인지 판단하기.
-	1) 복수의 IMAGE_SECTION_HEADER의 offset 파악하기.
-	2) 위의 offset으로 Characteristics 판정하기.
-	3) WRITE인 Characteristics은 offset 보존하기.
-	4) WRITE인 진입점 섹션의 offset 찾기
-	5) WRITE인 진입점 섹션의 offset에서 118byte 만큼 엔트로피 구해서 6.85이상이면 파일명과 경로 기록하기.
-	
-startReadAll함수
-개발 단계에서 모든 바이너리 엔트로피를 구하는 메소드이다.
+1. MZ 시그니쳐를 가지고 있는가?
+2. 진입점 색션을 찾는다.
+	1) address of entry point(RVA)가 속한 section구하기
+		(1) IMAGE_SECTION_HEADER의 RVA 항목값을 구한다.
+		(2) IMAGE_SECTION_HEADER의 Virtual Size 항목값을 구한다.
+		(3) IMAGE_SECTION_HEADER의 RVA 항목값 <= address of entry point(RVA) < IMAGE_SECTION_HEADER의 Virtual Size 항목값
+		(loop) 모든 IMAGE_SECTION_HEADER에서 위의 (1)~(3)을 반복한다.
+3. 진입점 색션이 write 속성인가?
+	1) IMAGE_SECTION_HEADER의 Characteristics attribute의 IMAGE_SCN_MEM_WRITE를 찾는다.
+	2) IMAGE_SCN_MEM_WRITE가 0x80이상인지 판단하기.
+4. write 속성이면, 진입점 색션의 entropy를 계산한다.
+5. entropy가 6.85이상이면 패킹이라고 판단한다.
 
-startNoEntropyCheck함수
-엔트로피 상관없이 전부 진입점 검사. 개발용 메소드
 '''
 
 class PackingTest():
@@ -30,7 +26,6 @@ class PackingTest():
 		print('This is PackingTest.');
 		self.packingInfo = {'entropies': 0, 'packedFile': ''};
 		
-
 	# 판단시작
 	def start(self, fileList, pbar, timerCounter):
 		packingInfoList = [];
@@ -40,14 +35,18 @@ class PackingTest():
 			currentLoopCounter += 1;
 		
 			fh = open(file, 'rb');
-			if not self.checkMZSignature(fh): fh.close(); continue;	# PE파일이 아니면 종료
+			if not self.checkMZSignature(fh): fh.close(); currentLoopCounter += 1; continue;	# MZ 시그니쳐를 가지고 있는가? 아니면 종료
 			
 			IMAGE_DOS_HEADER_offset = 0;
 			IMAGE_NT_HEADERS_offset = self.getIMAGE_NT_HEADERS_offset(fh);
-			entryPointOffsets = self.getEntryPointOffset(fh, IMAGE_NT_HEADERS_offset);
-			if entryPointOffsets == 0: fh.close(); continue;	# WRITE 권한이 있는 섹션이 없으므로 종료
+			IMAGE_OPTIONAL_HEADER_offset = IMAGE_NT_HEADERS_offset + 24;
+			addressOfEntryPoint = self.getAddressOfEntryPoint(fh, IMAGE_OPTIONAL_HEADER_offset);
 			
-			entropy = self.getEntropys(fh, entryPointOffsets); 
+			entryPointSectionOffset = self.getEntryPointSection(fh, IMAGE_NT_HEADERS_offset, addressOfEntryPoint);	# 진입점 색션의 offset을 찾는다.
+			
+			if not self.hasWriteAttribute(fh, entryPointSectionOffset): fh.close(); currentLoopCounter += 1; continue;	# 진입점 색션이 write 속성인가? 아니면 종료
+			
+			entropy = self.getEntropy(fh, entryPointSectionOffset); 	# write 속성이면, 진입점 색션의 entropy를 계산한다.
 			if entropy >= 6.85:
 				packingInfo = {'entropies': 0, 'packedFile': ''};
 				packingInfo['entropies'] = entropy;
@@ -55,7 +54,8 @@ class PackingTest():
 			else:
 				packingInfo = {'entropies': 0, 'packedFile': ''};
 				packingInfo['entropies'] = entropy;
-				packingInfo['packedFile'] = 'x';
+				packingInfo['packedFile'] = file;	# 디버깅용
+# 				packingInfo['packedFile'] = 'x';	# release용 
 			
 			packingInfoList.append(packingInfo);
 				
@@ -66,59 +66,6 @@ class PackingTest():
 
 		return packingInfoList; 	
 	
-	# 엔트로피 상관없이 전부 진입점 검사는 개발용 메소드
-	def startNoEntropyCheck(self, fileList, pbar, timerCounter):
-		packingInfoList = [];
-		currentLoopCounter = 0;		
-		
-		for file in fileList:
-			currentLoopCounter += 1;
-		
-			fh = open(file, 'rb');
-			if not self.checkMZSignature(fh): fh.close(); continue;	# PE파일이 아니면 종료
-			
-			IMAGE_DOS_HEADER_offset = 0;
-			IMAGE_NT_HEADERS_offset = self.getIMAGE_NT_HEADERS_offset(fh);
-			entryPointOffsets = self.getEntryPointOffset(fh, IMAGE_NT_HEADERS_offset);
-			if entryPointOffsets == 0: fh.close(); continue;	# WRITE 권한이 있는 섹션이 없으므로 종료
-			
-			entropy = self.getEntropys(fh, entryPointOffsets); 
-
-			packingInfo = {'entropies': 0, 'packedFile': ''};
-			packingInfo['entropies'] = entropy;
-			packingInfo['packedFile'] = file;
-			
-			packingInfoList.append(packingInfo);
-				
-			timerCounter = 100 * currentLoopCounter / len(fileList);
-			pbar.setValue(timerCounter);
-			
-			fh.close();  # 파일 닫기
-
-		return packingInfoList; 
-	
-	# 개발 단계에서 모든 바이너리 엔트로피를 구하는 메소드이다.
-	def startReadAll(self, fileList, pbar, timerCounter):
-
-		packingInfoList = [];
-		currentLoopCounter = 0;
-
-		for file in fileList:
-			currentLoopCounter += 1;
-		
-			fh = open(file, 'rb');
-			packingInfo = {'entropies': 0, 'packedFile': ''};
-			packingInfo['entropies'] = self.readAll(fh);
-			packingInfo['packedFile'] = file;
-			packingInfoList.append(packingInfo);
-			
-			timerCounter = 100 * currentLoopCounter / len(fileList);
-			pbar.setValue(timerCounter);
-			
-		fh.close();  # 파일 닫기
-		
-		return packingInfoList; 
-		
 	# MZ signature를 확인해서 PE파일인지 확인한다.
 	def checkMZSignature(self, fh):
 		fh.seek(0);
@@ -132,18 +79,18 @@ class PackingTest():
 	# IMAGE_NT_HEADERS의 offset을 구한다.
 	def getIMAGE_NT_HEADERS_offset(self, fh):	
 		fh.seek(60);
-		return self.getBytesStringValue(4, fh.read(4));
+		return self.getBytesIntValue(4, fh.read(4));
 	
 	# IMAGE_FILE_HEADER에서 IMAGE_SECTION_HEADER의 총 개수를 구한다.
 	def getSectionHeadersNumber(self, fh):
 		fh.seek(self.getIMAGE_NT_HEADERS_offset(fh)+6);
-		return self.getBytesStringValue(2, fh.read(2));
+		return self.getBytesIntValue(2, fh.read(2));
 	
 	# 첫번째 IMAGE_NT_HEADERS의 offset을 찾는다.
-	def getIMAGE_SECTION_HEADER_offset(self, fh, IMAGE_NT_HEADER_offset):
+	def getInitialIMAGE_SECTION_HEADER_offset(self, fh, IMAGE_NT_HEADER_offset):
 		sizeOfOptionalHeaderOffset = IMAGE_NT_HEADER_offset+20;
 		fh.seek(sizeOfOptionalHeaderOffset);
-		sizeOfOptionalHeader = self.getBytesStringValue(2, fh.read(2));
+		sizeOfOptionalHeader = self.getBytesIntValue(2, fh.read(2));
 		first = IMAGE_NT_HEADER_offset+24+sizeOfOptionalHeader;
 		return first;
 		
@@ -151,48 +98,77 @@ class PackingTest():
 	def getCharacteristics(self, fh, IMAGE_SECTION_HEADER_offset):
 		IMAGE_SCN_MEM_WRITE_offset = IMAGE_SECTION_HEADER_offset + 36 + 3;
 		fh.seek(IMAGE_SCN_MEM_WRITE_offset);
-		return self.getBytesStringValue(1, fh.read(1));
+		return self.getBytesIntValue(1, fh.read(1));
 
-	# IMAGE_SCN_MEM_WRITE의 값이 존재하는 offset을 모은 리스트를 반환한다.
-	def getEntryPointOffset(self, fh, IMAGE_NT_HEADERS_offset):
-		_IMAGE_SECTION_HEADER_offsets = [];
-		
-		initialOffset = self.getIMAGE_SECTION_HEADER_offset(fh, IMAGE_NT_HEADERS_offset);
+	# 진입점 색션을 찾는다.
+	def getEntryPointSection(self, fh, IMAGE_NT_HEADERS_offset, addressOfEntryPoint):
+		currentOffset = self.getInitialIMAGE_SECTION_HEADER_offset(fh, IMAGE_NT_HEADERS_offset);	# 구 getInitialIMAGE_SECTION_HEADER_offset()를 이름만 바꿔서 사용.
 		i = 0;
-		sectionsCounter = self.getSectionHeadersNumber(fh);
-		hex80 = int('0x80', 16);
 		
-		while i < sectionsCounter:
-			if self.getCharacteristics(fh, initialOffset) >= hex80:
-				fh.seek(initialOffset + 20);
-				
-				_IMAGE_SECTION_HEADER_offsets.append(self.getBytesStringValue(4, fh.read(4)));
-				print('WRITE 있다.');
+		sectionsCounter = self.getSectionHeadersNumber(fh);	#	구 함수 그대로 사용
+
+		while(i < sectionsCounter):
+			rva = self.getRVAAttibute(fh, currentOffset);
+			virtualSize = self.getVirtualSize(fh, currentOffset);
+
+			if(rva <= addressOfEntryPoint and addressOfEntryPoint < rva + virtualSize):
+				break;
 			else:
-				print('WRITE 없다.');
-			initialOffset += 40;
-			i += 1;
-			
-		return _IMAGE_SECTION_HEADER_offsets;
+				currentOffset += 40;
+				i += 1;
+		return currentOffset;
 	
-	# Characteristics가 WRITE인 SECTION의 offset에서 118 byte만큼의 엔트로피값들을 구한다.
-	def getEntropys(self, fh, entryPointOffsets):
+	# IMAGE_SECTION_HEADER에서 RVA attribute를 구한다.
+	def getRVAAttibute(self, fh, IMAGE_SECTION_HEADER_offset):
+		fh.seek(IMAGE_SECTION_HEADER_offset + 12);
+		return self.getBytesIntValue(4, fh.read(4));
+		
+	# IMAGE_SECTION_HEADER에서 virtual size attribute를 구한다.
+	def getVirtualSize(self, fh, IMAGE_SECTION_HEADER_offset):
+		fh.seek(IMAGE_SECTION_HEADER_offset+8);
+		return self.getBytesIntValue(4, fh.read(4));
+		
+	# IMAGE_OPTIONAL_HEADER에서 addressOfEntryPoint를 구한다.
+	def getAddressOfEntryPoint(self, fh, IMAGE_OPTIONAL_HEADER_offset):
+		fh.seek(IMAGE_OPTIONAL_HEADER_offset+16);
+		return self.getBytesIntValue(4, fh.read(4)); 
+	
+	# 진입점 색션이 write 속성인가?
+	def hasWriteAttribute(self, fh, entryPointSectionOffset):
+		hex80 = int('0x80', 16);
+		if self.getCharacteristics(fh, entryPointSectionOffset) >= hex80:
+			print('WRITE 있다.');
+			return True;
+		else:
+			print('WRITE 없다.');
+			return False;
+	
+	# write 속성이면, 진입점 색션의 entropy를 계산한다.
+	def getEntropy(self, fh, entryPointSectionOffset):
 		bytesStr = "";
 		readStr = "";
 		entropy = 0;
 		
-		for i in entryPointOffsets:
-			fh.seek(i);
+		pointerToRawData = entryPointSectionOffset + 20; # 진입점 색션의 시작 file offset
+		sizeOfRawData = entryPointSectionOffset + 16;	# 진입점 색션의 size를 담은 file offset
+		
+		fh.seek(pointerToRawData);
+		startOffset = self.getBytesIntValue(4, fh.read(4));
+		
+		fh.seek(sizeOfRawData);
+		sectionSize = self.getBytesIntValue(4, fh.read(4));
+
+		fh.seek(startOffset);
+		readStr = fh.read(sectionSize);
+		readStrLen = len(readStr);
+		
+		for i in range(readStrLen):
+			bytesStr += chr(readStr[i]);
 			
-			readStr = fh.read(118);
-			readStrLen = len(readStr);
-			for i in range(readStrLen):
-				bytesStr += chr(readStr[i]);
-				
-			p, lns = Counter(bytesStr), float(len(bytesStr));
-			entropy = -sum( count/lns * math.log(count/lns, 2) for count in p.values());
-			if entropy >= 6.85:
-				return entropy;
+		p, lns = Counter(bytesStr), float(len(bytesStr));
+		entropy = -sum( count/lns * math.log(count/lns, 2) for count in p.values());
+		if entropy >= 6.85:
+			return entropy;
 
 		return entropy;
 
@@ -216,7 +192,7 @@ class PackingTest():
 		return entropy;
 	
 	# open(rb)의 결과인 bytes 타입의 값을 Little endian -> Big endian으로 바꾸고  DEC int로 바꾼다. 
-	def getBytesStringValue(self, bytesStrLength, bytesStr):
+	def getBytesIntValue(self, bytesStrLength, bytesStr):
 		if bytesStrLength == 1:
 			hexStr = bytesStr.hex();
 			return int(hexStr, 16);
